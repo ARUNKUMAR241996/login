9997285153cc51758f64d13f2e7ef8bbfc9cf03f
Arunkumar g <33564276+ARUNKUMAR241996@users.noreply.github.com>
2019-12-16 17:09:21 +0530

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php include "templates/header.php"; ?>
+
+<ul>
+	<li><a href="create.php"><strong>REGISTRATION</strong></a></li>
+	<li><a href="read.php"><strong>LOGIN</strong></a></li>
+	<li><a href="login.php"><strong>LOGIN here</strong></a></li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
